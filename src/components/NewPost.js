be7108f1d1060c47582a2f3446ff483a87471063
Alcarian <floa2@msn.com>
2023-06-08 17:36:34 +0200

    commit 2

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -1,5 +1,27 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
-export default function NewPost() {
-  return <div>NewPost</div>;
+export default function NewPost(userId) {
+  const [message, setMessage] = useState("");
+
+  const handleForm = (e) => {
+    e.preventDefault();
+
+    axios.post(`${process.env.REACT_APP_API_URL}/post/`, {
+      message,
+      author: userId,
+    });
+    setMessage("");
+  };
+
+  return (
+    <form onSubmit={(e) => handleForm(e)} className="new-post-container">
+      <textarea
+        placeholder="Quoi de neuf ?"
+        onChange={(e) => setMessage(e.target.value)}
+        value={message}
+      ></textarea>
+      <input type="submit" value="Envoyer" />
+    </form>
+  );
 }
