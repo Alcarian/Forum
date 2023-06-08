5c3bacf43e160e8ec2c27d2a04c160af24e971dd
Alcarian <floa2@msn.com>
2023-06-08 15:23:34 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import NewPost from "./components/NewPost";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Le forum d'Alcarian</h1>
+      <div>
+        <h3>Qu'avez vous à dire ?</h3>
+      </div>
+      <NewPost />
     </div>
   );
 }
-
-export default App;
